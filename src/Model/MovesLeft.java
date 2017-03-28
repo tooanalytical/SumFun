589d62417d4997cd4cb85d3fff57a5fa5d6c3014
josephdoctor <jhdoct@gmail.com>
2017-03-28 12:28:54 -0400

    Score & MovesLeft classes not implement Serializable

--- src/Model/MovesLeft.java
+++ src/Model/MovesLeft.java
@@ -1,8 +1,9 @@
 package Model;
 
+import java.io.Serializable;
 import java.util.Observable;
 
-public class MovesLeft extends Observable {
+public class MovesLeft extends Observable implements Serializable {
 
 
 
--- src/Model/Score.java
+++ src/Model/Score.java
@@ -1,8 +1,9 @@
 package Model;
 
+import java.io.Serializable;
 import java.util.Observable;
 
-public class Score extends Observable {
+public class Score extends Observable implements Serializable {
 
 
 
