e794de51abeb0c002e104da29a192fe661930697
josephdoctor <jhdoct@gmail.com>
2017-03-27 20:51:51 -0400

    Added scores & moves left labels to UntimedBoard class

--- src/View/UntimedBoard.java
+++ src/View/UntimedBoard.java
@@ -14,9 +14,10 @@ public class UntimedBoard extends JFrame implements Observer {
     private final int WINDOW_HEIGHT = 800;
     private final int numRows, numColumns;
 
-    private JPanel pnlMaster, pnlGame, pnlQueue;
+    private JPanel pnlMaster, pnlGame, pnlInfo;
     private JButton[][] tileButtons;
     private JLabel[] queueLabels;
+    private JLabel lblScore, lblMovesLeft;
 
     private Tile[][] tiles;
     private Queue queue;
@@ -31,8 +32,6 @@ public class UntimedBoard extends JFrame implements Observer {
         this.queue = queue;
         this.score = score;
         this.movesLeft = movesLeft;
-        tileButtons = new JButton[numRows][numColumns];
-        queueLabels = new JLabel[5];
         addObservers();
 
         setTitle("Sum Fun!");
@@ -57,18 +56,19 @@ public class UntimedBoard extends JFrame implements Observer {
 
     private void buildMasterPanel(){
         buildGamePanel();
-        buildQueuePanel();
+        buildInfoPanel();
 
         pnlMaster = new JPanel();
         pnlMaster.setLayout(new GridLayout(1, 2));
 
         pnlMaster.add(pnlGame);
-        pnlMaster.add(pnlQueue);
+        pnlMaster.add(pnlInfo);
     }
 
     private void buildGamePanel(){
         pnlGame = new JPanel();
         pnlGame.setLayout(new GridLayout(numRows, numColumns));
+        tileButtons = new JButton[numRows][numColumns];
 
         // instantiates two-dimensional array of tile buttons & adds tile buttons to panel
         for(int r = 0; r < numRows; r++){
@@ -94,17 +94,51 @@ public class UntimedBoard extends JFrame implements Observer {
         }
     }
 
-    private void buildQueuePanel(){
-        pnlQueue = new JPanel();
-        pnlQueue.setLayout(new GridLayout(5, 1));
+    // this method needs cleaned up
+    private void buildInfoPanel(){
+        pnlInfo = new JPanel();
+        pnlInfo.setLayout(new GridLayout(1, 2));
+        queueLabels = new JLabel[5];
+        // instead of setting score to 0, use method from score object
+        lblScore = new JLabel("0", SwingConstants.CENTER);
+        lblScore.setFont(new Font("Arial", Font.PLAIN, 20));
+        // instead of setting moves left to 50, use method from movesLeft object
+        lblMovesLeft = new JLabel(Integer.toString(50), SwingConstants.CENTER);
+        lblMovesLeft.setFont(new Font("Arial", Font.PLAIN, 20));
 
-        // instantiates array of queue labels & adds queue labels to panel
+        // creates queue panel & adds panel to info panel
+        // instantiates array of queue labels & adds queue labels to queue panel
+        JPanel pnlQueue = new JPanel();
+        pnlQueue.setLayout(new GridLayout(5, 1));
         int[] temp = queue.getQueue();
         for(int i = 0; i < 5; i++){
             queueLabels[i] = new JLabel(Integer.toString(temp[i]), SwingConstants.CENTER);
             queueLabels[i].setFont(new Font("Arial", Font.BOLD, 30));
             pnlQueue.add(queueLabels[i]);
         }
+        pnlInfo.add(pnlQueue);
+
+        // creates scoremoves panel & adds panel to info panel
+        // creates score panel & moves panel & adds both panels to scoremoves panel
+        JPanel pnlScoreMoves = new JPanel();
+        pnlScoreMoves.setLayout(new GridLayout(2, 1));
+        JPanel pnlScore = new JPanel();
+        pnlScore.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        JLabel lblScoreDesc = new JLabel("SCORE: ", SwingConstants.CENTER);
+        lblScoreDesc.setFont(new Font("Arial", Font.PLAIN, 20));
+        pnlScore.add(lblScoreDesc, gbc);
+        pnlScore.add(lblScore, gbc);
+        pnlScoreMoves.add(pnlScore);
+        JPanel pnlMoves = new JPanel();
+        pnlMoves.setLayout(new GridBagLayout());
+        JLabel lblMovesDesc = new JLabel("MOVES LEFT: ", SwingConstants.CENTER);
+        lblMovesDesc.setFont(new Font("Arial", Font.PLAIN, 20));
+        pnlMoves.add(lblMovesDesc, gbc);
+        pnlMoves.add(lblMovesLeft, gbc);
+        pnlScoreMoves.add(pnlMoves);
+        pnlInfo.add(pnlScoreMoves);
+
     }
 
     public void update(Observable o, Object arg){
