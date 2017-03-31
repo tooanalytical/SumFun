33a3cd85d3154418f91306ee8b51f59da91ddbc2
josephdoctor <jhdoct@gmail.com>
2017-03-31 15:01:21 -0400

    Removed unnecessary logic from actionPerformed and isHit methods

--- src/Control/TileListener.java
+++ src/Control/TileListener.java
@@ -23,7 +23,6 @@ public class TileListener implements ActionListener {
 
     public void actionPerformed(ActionEvent e){
         JButton btn = (JButton) e.getSource();
-        Tile selectedTile= new Tile(true);
         //gets the row and column the button resides in
         int row = (int) btn.getClientProperty("row");
         int column = (int) btn.getClientProperty("col");
@@ -39,7 +38,7 @@ public class TileListener implements ActionListener {
 
             //if the button is empty, we need to see if the tile is empty
             int willRemove;
-            willRemove=isHit(selectedTile, row, column, value);
+            willRemove=isHit(row, column, value);
             //set the button to the queue value (will update the displayed value on board)
             //btn.setText(""+value);
             tiles[row][column].setValue(value);
@@ -112,80 +111,76 @@ public class TileListener implements ActionListener {
 
 
     // determines if move results in eliminated tiles/is a hit
-    private int isHit(Tile selectedTile, int row, int column, int value){
+    private int isHit(int row, int column, int value){
 
         // determines if selectedTile is hit
         // updates flag
 
         int removeTiles=0;
 
-        if (selectedTile.isEmpty()){
-            //tile placement is valid
+        //check how many neighbors it has
 
-            //check how many neighbors it has
-
-            //find the location of the selectedTile in the array
+        //find the location of the selectedTile in the array
 
 
-           /*
-            arrayTile[i+1][j] exists (right neighbor)
-            arrayTile[i-1][j] exists (left neighbor)
-            arraytile[i][j+1] exists (bottom neighbor)
-            arrayTile[i][j-1] exists (top neighbor)
-            arrayTile[i-1][j-1] exists (top left neighbor)
-            arrayTile[i+1][j-1] exists (top right neighbor)
-            arrayTile[i-1][j+1] exists (bottom left neighbor)
-            arrayTile[i+1][j+1] exists (bottom right neighbor)*/
+       /*
+        arrayTile[i+1][j] exists (right neighbor)
+        arrayTile[i-1][j] exists (left neighbor)
+        arraytile[i][j+1] exists (bottom neighbor)
+        arrayTile[i][j-1] exists (top neighbor)
+        arrayTile[i-1][j-1] exists (top left neighbor)
+        arrayTile[i+1][j-1] exists (top right neighbor)
+        arrayTile[i-1][j+1] exists (bottom left neighbor)
+        arrayTile[i+1][j+1] exists (bottom right neighbor)*/
 
-           int sum=0, neighborCount=0;
-            int prevRow=row-1, nextRow=row+1, prevColumn=column-1, nextColumn=column+1;
+       int sum=0, neighborCount=0;
+       int prevRow=row-1, nextRow=row+1, prevColumn=column-1, nextColumn=column+1;
 
-           if(nextRow<9 && (!tiles[nextRow][column].isEmpty())){
-               sum+= tiles[nextRow][column].getValue();
-               neighborCount++;
-           }
+       if(nextRow<9 && (!tiles[nextRow][column].isEmpty())){
+           sum+= tiles[nextRow][column].getValue();
+           neighborCount++;
+       }
 
-           if (prevRow>0 && (!tiles[prevRow][column].isEmpty())){
-               sum+= tiles[prevRow][column].getValue();
-               neighborCount++;
-           }
+       if (prevRow>0 && (!tiles[prevRow][column].isEmpty())){
+           sum+= tiles[prevRow][column].getValue();
+           neighborCount++;
+       }
 
-           if (nextColumn<9 && (!tiles[row][nextColumn].isEmpty())){
-               sum+= tiles[row][nextColumn].getValue();
-               neighborCount++;
-           }
+       if (nextColumn<9 && (!tiles[row][nextColumn].isEmpty())){
+           sum+= tiles[row][nextColumn].getValue();
+           neighborCount++;
+       }
 
-           if (prevColumn>0 && (!tiles[row][prevColumn].isEmpty())){
-               sum+= tiles[row][prevColumn].getValue();
-               neighborCount++;
-           }
+       if (prevColumn>0 && (!tiles[row][prevColumn].isEmpty())){
+           sum+= tiles[row][prevColumn].getValue();
+           neighborCount++;
+       }
 
-           if ((nextRow<9 && prevColumn>0) && (!tiles[nextRow][prevColumn].isEmpty())){
-               sum+= tiles[nextRow][prevColumn].getValue();
-               neighborCount++;
-           }
+       if ((nextRow<9 && prevColumn>0) && (!tiles[nextRow][prevColumn].isEmpty())){
+           sum+= tiles[nextRow][prevColumn].getValue();
+           neighborCount++;
+       }
 
-           if ((prevRow>0 && nextColumn<9) && (!tiles[prevRow][nextColumn].isEmpty())){
-               sum+= tiles[prevRow][nextColumn].getValue();
-               neighborCount++;
-           }
+       if ((prevRow>0 && nextColumn<9) && (!tiles[prevRow][nextColumn].isEmpty())){
+           sum+= tiles[prevRow][nextColumn].getValue();
+           neighborCount++;
+       }
 
-           if ((prevRow>0 && prevColumn>0) && (!tiles[prevRow][prevColumn].isEmpty())){
-               sum+= tiles[prevRow][prevColumn].getValue();
-               neighborCount++;
-           }
+       if ((prevRow>0 && prevColumn>0) && (!tiles[prevRow][prevColumn].isEmpty())){
+           sum+= tiles[prevRow][prevColumn].getValue();
+           neighborCount++;
+       }
 
-           if ((nextRow<9 && nextColumn<9) && (!tiles[nextRow][nextColumn].isEmpty())){
-               sum+= tiles[nextRow][nextColumn].getValue();
-               neighborCount++;
-           }
+       if ((nextRow<9 && nextColumn<9) && (!tiles[nextRow][nextColumn].isEmpty())){
+           sum+= tiles[nextRow][nextColumn].getValue();
+           neighborCount++;
+       }
 
-           int mod=sum%10;
+       int mod=sum%10;
 
-           if (value==mod){
-               removeTiles=neighborCount;
-           }
-        }
+       if (value==mod){
+           removeTiles=neighborCount;
+       }
 
 
         return removeTiles;
