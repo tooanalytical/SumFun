870c645f38f35bf3b53f73812d5bf4661698012e
josephdoctor <jhdoct@gmail.com>
2017-04-10 11:38:10 -0400

    Created abstract Board class & moved much of the code from UntimedBoard class over Updated UntimedBoard class so it now extends Board Created TimedBoard class which extends Board
    NOTE: Board class still needs abstract methods which will be used in the UntimedBoard & TimedBoard classes (see comments in code)

---
+++ src/View/TimedBoard.java
@@ -0,0 +1,11 @@
+package View;
+
+import Model.*;
+
+public class TimedBoard extends Board {
+
+    public TimedBoard(int numRows, int numColumns, Game game){
+        super(numRows, numColumns, game);
+    }
+
+}
